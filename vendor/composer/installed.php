8a644b9ef62e278dd393723a3084aa6449aa7920
Dudo1985 <d.curvino@gmail.com>
2023-05-19 12:19:56 +0200

    included vendor/composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dudo1985/comments-not-replied-to',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '311dbc1aa15d70b0de1a4aa38c3679f936e91d08',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dudo1985/comments-not-replied-to' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '311dbc1aa15d70b0de1a4aa38c3679f936e91d08',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.5.8',
+            'version' => '2.5.8.0',
+            'reference' => 'bbbcf3d976a1fd90355037b163746fdca6c93a4c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
